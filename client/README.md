5c016fe980c0f0ee2098054deb5bdbf5271c1480
Priyav Kaneria <priyavkaneria@gmail.com>
2023-02-06 14:22:46 +0530

    Tailwind colors config setup

--- client/README.md
+++ client/README.md
@@ -1,4 +1,7 @@
-# Getting Started with Create React App
+# Renaissance 2023
+---
+
+## Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,54 @@
 /** @type {import('tailwindcss').Config} */
+function withOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}), ${opacityValue})`;
+    }
+    return `rgb(var(${variableName}))`;
+  };
+}
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      xs: "475px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      xlsm: { min: "0px", max: "450px" },
+    },
+    extend: {
+      colors: {
+        linkedin: "#2867B2",
+        twitter: "#1DA1F2",
+        facebook: "#4267B2",
+        instagram: "#e95950",
+        custom: {
+          accent: withOpacity("--color-accent"),
+          secondary: withOpacity("--color-secondary"),
+          dark: withOpacity("--color-dark"),
+          light: withOpacity("--color-light"),
+        },
+      },
+      backgroundColor: {
+        custom: {
+          accent: withOpacity("--bg-accent"),
+          dark: withOpacity("--bg-dark"),
+          light: withOpacity("--bg-light"),
+        },
+      }, //usage: bg-custom-dark
+      borderColor: {
+        custom: {
+          accent: withOpacity("--border-accent"),
+          primary: withOpacity("--border-primary"),
+          secondary: withOpacity("--border-secondary"),
+        },
+      }, //usage: bg-custom-primary
+    },
   },
   plugins: [],
 }
