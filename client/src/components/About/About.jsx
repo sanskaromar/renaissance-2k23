00071c83f649d4e5a90032088588164c8cb84288
Sanchali Sharma <sharmasanchali06@gmail.com>
2023-02-08 21:39:25 +0530

    created count up numbers.
    animated numbers to count up when scrolled.

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import CountUp from "react-countup";
+import ScrollTrigger from "react-scroll-trigger";
 
 function About() {
+  //to set state for numbers counting up on scrolling
+  const [counterOn, setCounterOn] = useState(false);
+
   return (
     <div>
+      {/* div for the main section */}
       <div className="relative bg-[#354F52] h-screen">
         <div className="font-['Raleway'] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl sm:text-6xl text-left text-[#84A98C]">
           RENAISSANCE <br />
           <span className=" text-white">7.0</span>
         </div>
       </div>
+      {/* div for the about section */}
       <div className="md:flex md:flex-row md:items-center md:h-[30rem]">
-        <div className="relative h-[18rem] md:w-[140rem] md:h[rem]">
+        {/* div for seat confirmation and heading */}
+        <div className="relative h-[18rem] md:w-1/3 md:h-[30rem]">
           <div className="text-4xl font-['Raleway'] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-left text-[#354F52] ">
             ABOUT
             <br />
@@ -18,27 +26,69 @@ function About() {
             <br />
             WEEK
             <br />
-            <button className="text-left text-[8px] text-white bg-[#354F52] p-2">
+            <button className="text-left text-[12px] text-white bg-[#354F52] p-2 hover:bg-[#2F3E46] md:w-[10rem]">
               CONFIRM YOUR SEAT -{">"}
             </button>
           </div>
         </div>
-        <div>
-          <div className="text-left p-12">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac
-            facilisis nisi. Mauris eros metus, viverra in molestie sit amet,
-            pellentesque vel elit. Nunc eget elit et ex ullamcorper gravida sit
-            amet eu mi. Duis quis justo orci. Aliquam convallis cursus
-            ultricies. Vivamus vitae nunc et nisl scelerisque facilisis in nec
-            urna. Nam porta augue vitae lectus ornare blandit. Sed nec purus in
-            nunc tincidunt eleifend a vitae sem. Donec iaculis tellus vel est
-            molestie, ac mattis neque auctor. Ut gravida libero ac lectus
-            fringilla blandit. Quisque euismod lorem vel dolor fermentum
-            eleifend. Praesent consectetur tellus ante, in efficitur ex luctus
-            nec. Donec at diam tincidunt velit porttitor convallis. Cras dapibus
-            quam ipsum, nec ultricies leo pellentesque sed. Nulla facilisi.{" "}
+        {/* description  */}
+        <div className="md:w-2/3 md:h-[30rem] md:py-20 md:px-12">
+          <div className="w-full lg:w-3/4">
+            <div className="text-[#2F3E46] text-left p-12">
+              Renaissance is the annual entrepreneurial summit of MNNIT
+              Allahabad that provides a platform for aspiring entrepreneurs to
+              encourage the entrepreneurial journey of translating thoughts into
+              action. Renaissance includes a gamut of activities all of which
+              provide an avenue to harbor entrepreneurial skills and stimulate
+              discussion on ideas that possess the potential to turn the issues
+              that plague us into opportunities.{" "}
+            </div>
+            <ScrollTrigger
+              onEnter={() => setCounterOn(true)}
+              onExit={() => setCounterOn(false)}
+              className="text-[#354F52] flex flex-row px-12"
+            >
+              <div className="pb-12 w-1/3 text-left">
+                {counterOn && (
+                  <CountUp
+                    className="text-2xl"
+                    start={0}
+                    end={8}
+                    duration={1}
+                    delay={0}
+                  />
+                )}
+                +<br />
+                Events
+              </div>
+              <div className="pb-12 w-1/3  text-left">
+                {counterOn && (
+                  <CountUp
+                    className="text-2xl"
+                    start={0}
+                    end={9}
+                    duration={1}
+                    delay={0}
+                  />
+                )}
+                +<br />
+                Speakers
+              </div>
+              <div className="pb-12 w-1/3  text-left">
+                {counterOn && (
+                  <CountUp
+                    className="text-2xl"
+                    start={0}
+                    end={25}
+                    duration={1}
+                    delay={0}
+                  />
+                )}
+                +<br />
+                Sponsors
+              </div>
+            </ScrollTrigger>
           </div>
-          <div>numbers</div>
         </div>
       </div>
     </div>
