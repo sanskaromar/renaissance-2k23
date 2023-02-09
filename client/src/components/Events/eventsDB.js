9bea5de0aa28f9d973591a19e6a0985b8197e661
Cyber_Infernox <sayonkar@gmail.com>
2023-02-09 14:52:10 +0530

    Event images modified

--- client/src/components/Events/eventsDB.js
+++ client/src/components/Events/eventsDB.js
@@ -1,11 +1,10 @@
 import img1 from "../../assets/Events/One.jpg";
 import img2 from "../../assets/Events/Two.jpg";
+import img3 from "../../assets/Events/Three.jpg";
 import img4 from "../../assets/Events/Four.jpeg";
 import img5 from "../../assets/Events/Five.jpg";
 import img6 from "../../assets/Events/Six.jpg";
-import img7 from "../../assets/Events/Seven.jpg";
-import img8 from "../../assets/Events/Eight.jpg";
-import img9 from "../../assets/Events/Nine.jpeg";
+import img7 from "../../assets/Events/Seven.jpeg";
 
 const events = [
   {
@@ -17,57 +16,57 @@ const events = [
     body: "Provides an opportunity for aspiring entrepreneurs to propose their business ideas and get cash and mentoring from industry experts",
   },
   {
-    urlOne: img8,
+    urlOne: img7,
     urlTwo: img1,
-    urlThree: img2,
-    urlFour: img9,
+    urlThree: img4,
+    urlFour: img3,
     title: "E-TALK",
     body: "Gives a chance to experience the aura of exceptional entrepreneurs and industrialists and familiarize with the experience of those individuals through live chat and questioning",
   },
   {
-    urlOne: img7,
+    urlOne: img6,
     urlTwo: img4,
-    urlThree: img1,
-    urlFour: img2,
+    urlThree: img7,
+    urlFour: img5,
     title: "MOCK IPL AUCTION",
     body: "The most enthralling event where participants can make their dream squad by bidding on the players they want. If bets aren't placed carefully, the winner gets it all",
   },
   {
-    urlOne: img9,
-    urlTwo: img6,
-    urlThree: img4,
-    urlFour: img8,
+    urlOne: img1,
+    urlTwo: img3,
+    urlThree: img6,
+    urlFour: img2,
     title: "GAME OF STOCKS",
     body: "Without putting any money at risk, one can experience the thrills and spills of investing. Learn the fundamentals of the stock market while competing for rewards through hands-on study of economics and financial topics",
   },
   {
-    urlOne: img5,
-    urlTwo: img1,
-    urlThree: img7,
-    urlFour: img6,
+    urlOne: img4,
+    urlTwo: img3,
+    urlThree: img5,
+    urlFour: img7,
     title: "INTERNSHIP FAIR",
     body: "A recruiting event for aspirants, allowing them to obtain work experience in a professional setting and serving as a springboard for a variety of occupations. Apply for internship positions in a variety of fields, and get recruited!",
   },
   {
-    urlOne: img4,
-    urlTwo: img8,
+    urlOne: img3,
+    urlTwo: img1,
     urlThree: img2,
-    urlFour: img9,
+    urlFour: img6,
     title: "PANEL DISCUSSION",
     body: "Gain a better understanding of current trends, philosophies, and methodologies when industry veterans, business gurus, venture capitalists, and entrepreneurs debate and discuss ideas, perspectives, and trends together",
   },
   {
-    urlOne: img6,
-    urlTwo: img7,
-    urlThree: img5,
-    urlFour: img4,
+    urlOne: img4,
+    urlTwo: img6,
+    urlThree: img7,
+    urlFour: img5,
     title: "CONTENT-O-RAMA",
     body: "Content-o-Rama brings with itself the opportunity to experience the world of advertisements, content creation, and social media management",
   },
   {
-    urlOne: img4,
-    urlTwo: img2,
-    urlThree: img7,
+    urlOne: img3,
+    urlTwo: img4,
+    urlThree: img2,
     urlFour: img1,
     title: "NAYAK",
     body: "An event where the participants will be tested on their unique problem-solving approach to a business challenge",
