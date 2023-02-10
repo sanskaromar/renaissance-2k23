13dc5f759ed66ac90a5b714f2900a5ee9fa4ba4d
samjain233 <94921996+samjain233@users.noreply.github.com>
2023-02-10 10:53:59 +0530

    added responsiveness in mobile view

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.png";
 
 const navItems = [
@@ -10,7 +10,7 @@ const navItems = [
   "contact",
 ];
 
-const Hamburger = (
+const HamOpen = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
@@ -27,12 +27,31 @@ const Hamburger = (
   </svg>
 );
 
+const HamClose = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="white"
+    className="w-10 h-10"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 12.75l7.5-7.5 7.5 7.5m-15 6l7.5-7.5 7.5 7.5"
+    />
+  </svg>
+);
+
 function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
     // navbar component
-    <nav className="w-full bg-custom-dark flex flex-row z-50 fixed">
+    <nav className="w-full bg-custom-dark flex flex-row justify-between z-50 fixed">
       {/* logo  */}
-      <div className="logo px-12 py-8 flex justify-center items-center">
+      <div className="logo px-4 py-4 flex justify-center items-center w-1/2 h-1/2 xs:px-8 xs-py-8 md:w-full md:h-full">
         <a href="#">
           <img className="cursor-pointer" src={logo} alt="Renaissance Logo" />
         </a>
@@ -65,23 +84,44 @@ function Navbar() {
       </div>
 
       {/* Hamburger Navigation Menu  */}
-      <div className="md:hidden fixed top-0 left-0 right-0 p-1 w-screen h-[80%] flex justify-end items-end flex-col bg-custom-dark opacity-90 shadow-lg">
-        {Hamburger}
-        <ul className="m-0 p-0 h-full w-full text-custom-nav text-xs flex flex-col justify-center items-center">
-          {navItems.map((item) => (
-            <li className="mx-10 my-4 cursor-pointer" key={`link-${item}`}>
-              <a href="#" className="text-xl font-light text-custom-light">
-                {item.toUpperCase()}
-              </a>
-            </li>
-          ))}
+      <div className="md:hidden relative flex justify-center items-center cursor-pointer">
+        <div className="flex" onClick={() => setToggle(true)}>
+          {HamOpen}
+        </div>
+        {toggle && (
+          <div className="md:hidden fixed top-0 left-0 right-0 p-1 w-screen h-[85%] flex justify-end items-end flex-col bg-custom-dark opacity-90 shadow-lg">
+            <div className="flex p-3" onClick={() => setToggle(false)}>
+              {HamClose}
+            </div>
 
-          <li className="mx-10 my-10 p-4 cursor-pointer bg-custom-secondary">
-            <a href="#" className="text-xl font-bold text-custom-accent">
-              CONFIRM YOUR SEAT
-            </a>
-          </li>
-        </ul>
+            {/* logo */}
+            <div className="logo m-0 p-0 mb-5 w-full h-full  flex justify-center items-center">
+              <img
+                className="cursor-pointer w-1/2 "
+                src={logo}
+                alt="Renaissance Logo"
+              />
+            </div>
+
+            {/* Nav-items  */}
+            <ul className="m-0 p-0 h-full w-full text-custom-nav text-xs flex flex-col justify-center items-center">
+              {navItems.map((item) => (
+                <li className="mx-10 my-4 cursor-pointer" key={`link-${item}`}>
+                  <a href="#" className="text-xl font-light text-custom-light">
+                    {item.toUpperCase()}
+                  </a>
+                </li>
+              ))}
+
+              <li className="mx-10 my-10 p-4 cursor-pointer bg-custom-secondary">
+                <a href="#" className="text-xl font-bold text-custom-accent">
+                  CONFIRM YOUR SEAT
+                </a>
+              </li>
+            </ul>
+          </div>
+        )}
+        ;
       </div>
     </nav>
   );
