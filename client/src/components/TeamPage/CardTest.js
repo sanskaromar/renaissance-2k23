b5caaed28e65038c32fc95f724ffacb3a2023d9c
Sambhav-Sam <97016770+Sambhav-Sam@users.noreply.github.com>
2023-02-10 21:23:44 +0530

    Created TeamPage

---
+++ client/src/components/TeamPage/CardTest.js
@@ -0,0 +1,204 @@
+import React from "react";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+const CardTest = () => {
+  return (
+    <>
+      <h2 class="design">Design Team</h2>
+      <div class="box">
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1549417229-aa67d3263c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1548094878-84ced0f6896d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <FacebookIcon className="space" />
+              </a>
+              <a href="">
+                <LinkedInIcon className="space" />
+              </a>
+              <a href="">
+                <TwitterIcon className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CardTest;
