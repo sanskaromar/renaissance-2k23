67e354f6f5c6a2432c8e845ca6b9b9967e722a44
Aditya Raj Rai <aditya70544@icloud.com>
2023-02-12 00:30:04 +0530

    Added a working sample in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,41 @@ import "./App.css";
 import Sponsors from "./components/Sponsors/Sponsors";
 
 function App() {
-  return <></>;
+  const List = [
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+    "./SponsorsImages/ecellLogo.png",
+  ];
+  return <Sponsors ImageList={List} />;
 }
 
 export default App;
