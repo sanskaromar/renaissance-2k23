f3cbca444c9034b2c2cc99378e1f8a0887e9113d
PANDEYS432 <98703881+PANDEYS432@users.noreply.github.com>
2023-02-11 13:49:15 +0530

    modification in Testimonials component
    mapping of all testimonials done

---
+++ client/src/components/Testimonials/data.js
@@ -0,0 +1,49 @@
+import Rajeev from "./Rajeev.jpg";
+import chetanya from "./chetanya.jpg";
+import Gary from "./Gary.png";
+import Abhilekh from "./Abhilekh.jpg";
+import Mansi from "./Mansi.jpg";
+
+const people = [
+  {
+    id: 1,
+    image: Rajeev,
+    name: "Rajeev Tripathi",
+    title: "Director, MNNIT Allahabad",
+    headline: "Efficient Collaborating",
+    text: " I duly hope the legacy continues eternally and provide ample opportunities to cater and optimistic demeanor towards being an entrepreneur, surpassing the bars set by the previous endeavours.",
+  },
+  {
+    id: 2,
+    image: chetanya,
+    name: "Chetanya Golecha",
+    title: "Co-Founder, Water Float ",
+    headline: " Intuitive Design",
+    text: " I would like to give my personal congrats to the entire team of NIT Allahabad for putting up a wonderful event.The attendees were more than ecstatic than what I have ever seen anywhere else and it is this spirit that puts up a favourable stage for people like us to speak/perform.",
+  },
+  {
+    id: 3,
+    image: Gary,
+    name: "Gary Dalal",
+    title: "Director, Apptology FZCO",
+    headline: " Mindblowing Service",
+    text: " I would like to give my personal congrats to the entire team of NIT Allahabad for putting up a wonderful event.The attendees were more than ecstatic than what I have ever seen anywhere else and it is this spirit that puts up a favourable stage for people like us to speak/perform. ",
+  },
+  {
+    id: 4,
+    image: Abhilekh,
+    name: "Abhilekh Agarwal",
+    title: "CTO, Cube26",
+    headline: "  Great Effort",
+    text: "  It was a great effort by Team Renaissance. Greatly appreciated coming back to college. Your hard work and efforts has lead to such successful event. ",
+  },
+  {
+    id: 5,
+    image: Mansi,
+    name: "Mansi Matela",
+    title: "Head Strategy & New Initiatives, SilverPus",
+    headline: "  Thrilling Experience",
+    text: " I was thrilled to see such an amazing audience. The events were extraordinary and it surely made an impact to many aspiring entrepreneurs. A platform for the genesis of great ideas. ",
+  },
+];
+export default people;
