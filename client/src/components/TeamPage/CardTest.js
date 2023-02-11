9a073f448f4dc87a2d4970a5447ed33a7056e280
Sambhav-Sam <97016770+Sambhav-Sam@users.noreply.github.com>
2023-02-12 01:43:04 +0530

    Added CSS and ReactIcons

--- client/src/components/TeamPage/CardTest.js
+++ client/src/components/TeamPage/CardTest.js
@@ -1,8 +1,10 @@
 import React from "react";
-import FacebookIcon from "@mui/icons-material/Facebook";
-import LinkedInIcon from "@mui/icons-material/LinkedIn";
-import TwitterIcon from "@mui/icons-material/Twitter";
-
+import {
+  AiFillFacebook,
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+} from "react-icons/ai";
+import "./CardTest.css";
 const CardTest = () => {
   return (
     <>
@@ -28,13 +30,13 @@ const CardTest = () => {
             <div className="color"></div>
             <div className="links">
               <a href="">
-                <FacebookIcon className="space" />
+                <AiFillFacebook className="space" />
               </a>
               <a href="">
-                <LinkedInIcon className="space" />
+                <AiFillLinkedin className="space" />
               </a>
               <a href="">
-                <TwitterIcon className="space" />
+                <AiFillTwitterCircle className="space" />
               </a>
             </div>
           </div>
@@ -60,13 +62,13 @@ const CardTest = () => {
             <div className="color"></div>
             <div className="links">
               <a href="">
-                <FacebookIcon className="space" />
+                <AiFillFacebook className="space" />
               </a>
               <a href="">
-                <LinkedInIcon className="space" />
+                <AiFillLinkedin className="space" />
               </a>
               <a href="">
-                <TwitterIcon className="space" />
+                <AiFillTwitterCircle className="space" />
               </a>
             </div>
           </div>
@@ -92,44 +94,13 @@ const CardTest = () => {
             <div className="color"></div>
             <div className="links">
               <a href="">
-                <FacebookIcon className="space" />
+                <AiFillFacebook className="space" />
               </a>
               <a href="">
-                <LinkedInIcon className="space" />
+                <AiFillLinkedin className="space" />
               </a>
               <a href="">
-                <TwitterIcon className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <FacebookIcon className="space" />
-              </a>
-              <a href="">
-                <LinkedInIcon className="space" />
-              </a>
-              <a href="">
-                <TwitterIcon className="space" />
+                <AiFillTwitterCircle className="space" />
               </a>
             </div>
           </div>
@@ -154,13 +125,13 @@ const CardTest = () => {
             <div className="color"></div>
             <div className="links">
               <a href="">
-                <FacebookIcon className="space" />
+                <AiFillFacebook className="space" />
               </a>
               <a href="">
-                <LinkedInIcon className="space" />
+                <AiFillLinkedin className="space" />
               </a>
               <a href="">
-                <TwitterIcon className="space" />
+                <AiFillTwitterCircle className="space" />
               </a>
             </div>
           </div>
@@ -185,13 +156,44 @@ const CardTest = () => {
             <div className="color"></div>
             <div className="links">
               <a href="">
-                <FacebookIcon className="space" />
+                <AiFillFacebook className="space" />
               </a>
               <a href="">
-                <LinkedInIcon className="space" />
+                <AiFillLinkedin className="space" />
               </a>
               <a href="">
-                <TwitterIcon className="space" />
+                <AiFillTwitterCircle className="space" />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="all">
+          <div class="card">
+            <div class="imgBx">
+              <img
+                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                alt="images"
+              />
+            </div>
+            <div class="details">
+              <h2>
+                SomeOne Famous
+                <br />
+                <span>Director</span>
+              </h2>
+            </div>
+          </div>
+          <div className="info">
+            <div className="color"></div>
+            <div className="links">
+              <a href="">
+                <AiFillFacebook className="space" />
+              </a>
+              <a href="">
+                <AiFillLinkedin className="space" />
+              </a>
+              <a href="">
+                <AiFillTwitterCircle className="space" />
               </a>
             </div>
           </div>
