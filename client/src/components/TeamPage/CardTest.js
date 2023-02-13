7fcf93d29fc3d2933b2ee3421f987b9d5511e967
Sambhav-Sam <97016770+Sambhav-Sam@users.noreply.github.com>
2023-02-13 12:25:06 +0530

    Created Components

--- client/src/components/TeamPage/CardTest.js
+++ client/src/components/TeamPage/CardTest.js
@@ -1,222 +1,31 @@
 import React from "react";
-import {
-  AiFillFacebook,
-  AiFillTwitterCircle,
-  AiFillLinkedin,
-  AiFillYoutube,
-} from "react-icons/ai";
 import "./CardTest.css";
+import Details from "./Details";
+const people1 =
+  "https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60";
 const CardTest = () => {
   return (
     <>
       <h2 class="design">Design Team</h2>
       <div class="box">
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1549417229-aa67d3263c09?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1548094878-84ced0f6896d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="all">
-          <div class="card">
-            <div class="imgBx">
-              <img
-                src="https://images.unsplash.com/photo-1532123675048-773bd75df1b4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-                alt="images"
-              />
-            </div>
-            <div class="details">
-              <h2>
-                SomeOne Famous
-                <br />
-                <span>Director</span>
-              </h2>
-            </div>
-          </div>
-          <div className="info">
-            <div className="color"></div>
-            <div className="links">
-              <a href="">
-                <AiFillFacebook className="space" />
-              </a>
-              <a href="">
-                <AiFillTwitterCircle className="space" />
-              </a>
-              <a href="">
-                <AiFillLinkedin className="space" />
-              </a>
-              <a href="">
-                <AiFillYoutube className="space" />
-              </a>
-            </div>
-          </div>
-        </div>
+        <Details
+          image={people1}
+          name="Someone"
+          year="2020"
+          facebook="https://www.facebook.com/"
+          twitter="https://www.twitter.com/"
+          linkedin="https://www.linkedin.com/"
+          youtube="https://www.youtube.com/"
+        />
+        <Details
+          image={people1}
+          name="Someone"
+          year="2020"
+          facebook="https://www.facebook.com/"
+          twitter="https://www.twitter.com/"
+          linkedin="https://www.linkedin.com/"
+          youtube="https://www.youtube.com/"
+        />
       </div>
     </>
   );
