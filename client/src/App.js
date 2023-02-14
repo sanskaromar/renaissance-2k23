ddb205cdb7954596d471a72687a625129e0d9520
Chandni <chandnik96.40@gmail.com>
2023-02-14 18:08:55 +0530

    Minor changes in UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Speakers from "./components/Speakers/Speakers.jsx";
-import Gallery from "./components/Gallery/Gallery.jsx";
+// import Gallery from "./components/Gallery/Gallery.jsx";
 import Carousel from "./components/Gallery/Gallery.jsx";
 function App() {
   return (
