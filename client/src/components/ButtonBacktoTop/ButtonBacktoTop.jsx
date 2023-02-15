d45cb311160e9ddaa97507a03aabc15754993cf6
Aditya Raj Rai <aditya70544@icloud.com>
2023-02-15 18:35:05 +0530

    Implemented Back to top button

---
+++ client/src/components/ButtonBacktoTop/ButtonBacktoTop.jsx
@@ -0,0 +1,42 @@
+import { React, useState, useEffect } from "react";
+import { RiArrowDropUpLine } from "react-icons/ri";
+import "./ButtonBacktoTop.css";
+
+const ButtonBacktoTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const ScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 700;
+    const winScroll =
+      document.body.scrollTop || document.documentElement.scrollTop;
+
+    if (winScroll > heightToHideFrom) !isVisible && setIsVisible(true);
+    else setIsVisible(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
+  }, []);
+
+  return (
+    isVisible && (
+      <>
+        <div className="TopButton flex w-full  lg:items-end justify-end pr-8 pb-5 ">
+          <button
+            className="bg-[#CAD2C5] h-[47px] w-[47px] flex items-center justify-center shadow-2xl"
+            onClick={ScrollToTop}
+          >
+            <RiArrowDropUpLine className="text-[30px]" />
+          </button>
+        </div>
+      </>
+    )
+  );
+};
+
+export default ButtonBacktoTop;
