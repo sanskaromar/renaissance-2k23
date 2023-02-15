3e9a3b2cdba651951ea156b301b7664adf603db4
Cyber_Infernox <sayonkar@gmail.com>
2023-02-15 12:53:45 +0530

    Carousel design added

--- client/src/pages/Events/EPlan.jsx
+++ client/src/pages/Events/EPlan.jsx
@@ -1,6 +1,14 @@
 import Navbar from "../../components/Navbar/Navbar";
 import EventFooter from "./EventFooter";
+import "./Style/Event.css";
 import img from "../../assets/EventPage/eplan.svg";
+
+//Carousel
+import img1 from "../../assets/Events/One.jpg";
+import img2 from "../../assets/Events/Two.jpg";
+import img3 from "../../assets/Events/Three.jpg";
+import img4 from "../../assets/Events/Four.jpeg";
+import img5 from "../../assets/Events/Five.jpg";
 
 const EPlan = () => {
   return (
@@ -99,6 +107,62 @@ const EPlan = () => {
           </div>
         </div>
       </div>
+
+      <div className="carousel bg-custom-secondary pb-[70px] pt-[45px]">
+        <div className="text-center font-semibold text-4xl mb-[50px] tracking-wide">
+          GALLERY
+        </div>
+        <section className="mb-[40px]" id="slider">
+          <input type="radio" name="slider" id="s1" />
+          <input type="radio" name="slider" id="s2" />
+          <input
+            type="radio"
+            name="slider"
+            id="s3"
+            onChange={(e) => e.target.checked}
+          />
+          <input type="radio" name="slider" id="s4" />
+          <input type="radio" name="slider" id="s5" />
+
+          <label for="s1" id="slide1">
+            <img className="h-full w-full" src={img1} />
+          </label>
+
+          <label for="s2" id="slide2">
+            <img className="h-full w-full" src={img2} />
+          </label>
+
+          <label for="s3" id="slide3">
+            <img className="h-full w-full" src={img3} />
+          </label>
+
+          <label for="s4" id="slide4">
+            <img className="h-full w-full" src={img4} />
+          </label>
+
+          <label for="s5" id="slide5">
+            <img className="h-full w-full" src={img5} />
+          </label>
+        </section>
+        <div className="flex justify-center">
+          <button className="bg-custom-accent mt-5 px-3 py-3 pl-4 w-50 text-left rounded-sm font-semibold flex justify-left items-center hover:bg-custom-secondary transition ease-in-out duration-700">
+            CONFIRM YOUR SEAT
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="ml-1 w-7 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+              />
+            </svg>
+          </button>
+        </div>
+      </div>
+
       <EventFooter />
     </div>
   );
